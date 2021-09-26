e3ef7f7e9130771ae36977657eb9dd79cf5b0755
rexar <rexarkoexygen@gmail.com>
2021-09-26 15:13:02 +0400

    update

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ReSharper disable Unity.PreferAddressByIdToGraphicsParams
+
+// ReSharper disable InconsistentNaming
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D theRB;
+    public float jumpForce;
+
+    private bool isGrounded;
+    public LayerMask whatIsGround;
+    public Transform groundCheckPoint;
+
+    private bool canDoubleJump;
+
+    private Animator anim;
+    private SpriteRenderer theSR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        theSR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), theRB.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            }
+            else
+            {
+                if (canDoubleJump)
+                {
+                    theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
+        }
+
+        if (theRB.velocity.x < 0)
+        {
+            theSR.flipX = true;
+        } else if (theRB.velocity.x > 0)
+        {
+            theSR.flipX = false;
+        }
+        
+        anim.SetFloat("moveSpeed", System.Math.Abs(theRB.velocity.x));
+        anim.SetBool("isGrounded", isGrounded);
+    }
+}
